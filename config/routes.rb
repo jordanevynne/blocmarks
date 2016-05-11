1908a834f6625dc03189a977ae5c6bf03be4318d
Jordan Marcus <jordan.e.marcus@gmail.com>
2016-05-10 19:06:20 -0700

    add rspec files

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'topics/index'
-
-  get 'topics/show'
-
-  get 'topics/new'
-
-  get 'topics/edit'
 
   devise_for :users
+  resources :topics
+  
   get 'welcome/index'
 
   root 'welcome#index'
