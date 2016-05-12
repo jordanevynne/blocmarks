d7892f79dd2f6b3992e9baaf309d561f735ca1a9
Jordan Marcus <jordan.e.marcus@gmail.com>
2016-05-11 18:46:55 -0700

    add topics new view & specs

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -8,6 +8,20 @@ class TopicsController < ApplicationController
   end
 
   def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new
+    @topic.title = params[:topic][:title]
+
+    if @topic.save
+      flash[:notice] = "Topic was saved successfully."
+      redirect_to @topic
+    else
+      flash.now[:alert] = "There was an error saving the topic. Please try again."
+      render :new
+    end
   end
 
   def edit
--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe TopicsController, type: :controller do
-  let(:my_topic) { Topic.create!(title: "topic") }
+  let(:my_topic) { Topic.create(title: "topic") }
 
-  describe "GET #index" do
+  describe "GET index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
 
-    it "assigns [my_topic] to @topics" do
+    it "assigns my_topic to @topics" do
       get :index
       expect(assigns(:topics)).to eq([my_topic])
     end
   end
-end
 
 #  describe "GET #show" do
 #    it "returns http success" do
@@ -23,10 +22,39 @@ end
 #    end
 #  end
 #
-#  describe "GET #new" do
-#    it "returns http success" do
-#      get :new
-#      expect(response).to have_http_status(:success)
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET new" do
+    it "returns http success" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  it "instantiates @topic" do
+    get :new
+    expect(assigns(:topic)).not_to be_nil
+  end
+end
+
+# describe "TOPIC create" do
+#    it "increases the number of Topics by 1" do
+#      expect{topic :create, topic: {title: "topic"}}.to change(Topic,:count).by(1)
+#    end
+#
+#    it "assigns the new topic to @topic" do
+#      topic :create, topic: {title: "topic"}
+#      expect(assigns(:topic)).to eq Topic.last
+#    end
+#
+#    it "redirects to the new topic" do
+#      topic :create, topic: {title: "topic"}
+#      expect(response).to redirect_to Topic.last
 #    end
 #  end
 #
@@ -36,5 +64,4 @@ end
 #      expect(response).to have_http_status(:success)
 #    end
 #  end
-
 #end
