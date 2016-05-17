f31c3704ba30a1d790601ee0ff831e6b95a2a011
Jordan Marcus <jordan.e.marcus@gmail.com>
2016-05-16 19:09:44 -0700

    change routes.rb root to topics index

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
   resources :topics
   resources :bookmarks
 
-  get 'welcome/index'
-
-  root 'welcome#index'
+  root 'topics#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
