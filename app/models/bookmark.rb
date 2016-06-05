d059b4698e2be5e6b1c13bea12b23e9d48829236
Jordan Marcus <jordan.e.marcus@gmail.com>
2016-06-04 18:03:10 -0700

    add likes model & associations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,7 @@ class Bookmark < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
 
+  has_many :likes, dependent: :destroy
+
   validates_presence_of :user, :topic
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
          has_many :bookmarks, dependent: :destroy
+         has_many :likes, dependent: :destroy
        end
