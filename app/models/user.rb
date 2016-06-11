1f077e92e96b19c8446efc02b09d5a82c576e9db
Jordan Marcus <jordan.e.marcus@gmail.com>
2016-06-10 21:18:32 -0700

    user/bookmark fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
 
   has_many :topics
-  has_many :bookmarks, through: :topics, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  #has_many :bookmarks, through: :topics, dependent: :destroy
   has_many :likes, dependent: :destroy
 
   def liked(bookmark)
